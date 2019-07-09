92bb9498b0ee7e2feec02023f558807778140178
Lexxicus <lexx-03@mail.ru>
2019-07-09 19:39:17 +0500

    homework
    dobavili pravil'nii sintaksis jquery

--- app.rb
+++ app.rb
@@ -2,6 +2,24 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/visit/' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
@@ -10,12 +28,38 @@ get '/about' do
 	erb :about
 end
 
-get '/visit' do
-	erb :visit
-end
-
 get '/contacts' do
   erb :contacts
+end
+
+get '/login_form' do
+  erb :login_form
+end
+
+get '/logout' do
+  session.delete(:identity)
+  redirect to '/'
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+get '/visit' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+  erb :visit
+end
+
+post '/login/attempt' do
+  if params['username'] == 'admin' && params['password'] == 'secret'
+    session[:identity] = params['username']
+    where_user_came_from = session[:previous_url] || '/'
+    redirect to where_user_came_from
+  else
+    @pass_error = 'Sorry, you enter wrong login or password, try again '
+    halt erb(:login_form)
+  end
 end
 
 post '/visit' do
